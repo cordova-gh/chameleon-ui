7ab85fc238d28e19c493cc1bf02abb8dd205dea2
Gaia Gulinelli <ibryan2205@gmail.com>
2020-09-05 13:57:56 +0200

    add form and pagination json

--- src/pages/admin/company/Company.vue
+++ src/pages/admin/company/Company.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <title-page titolo= "Company"></title-page>
+    <router-link to="new" >New</router-link>
     <router-view></router-view>
   </div>
 </template>
--- src/pages/admin/company/pagination/CompanyPagination.vue
+++ src/pages/admin/company/pagination/CompanyPagination.vue
@@ -1,6 +1,25 @@
 <template>
     <div>
-        ciao sono company pagination
+        <ui-pagination
+        :config="configPagination"
+        :urlApi="urlApi"></ui-pagination>
     </div>
-
 </template>
+<script>
+import { API_COMPANY } from '../../../../services/constant-services';
+import UIPagination from '../../../../ui-components/UIPagination';
+import ConfigPagination from './CompanyPagination.json';
+
+export default {
+  name: 'UserPagination',
+  data() {
+    return {
+      urlAPi: API_COMPANY,
+      configPagination: ConfigPagination,
+    };
+  },
+  components: {
+    'ui-pagination': UIPagination,
+  },
+};
+</script>
