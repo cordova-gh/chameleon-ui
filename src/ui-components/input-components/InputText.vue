f83d80a90857628f83cd2adcc8e74654b27f3354
Geancarlo Franklin Cordova Aguero <ukg09573@kireygroup.com>
2020-09-16 18:27:46 +0200

    version not clear di inputext

--- src/ui-components/input-components/InputText.vue
+++ src/ui-components/input-components/InputText.vue
@@ -7,7 +7,7 @@
       :class="classCss"
       :readonly="readonlyAttr"
     />
-    <label for="inputEmail">Email address</label>
+    <label for="inputEmail">{{label}}</label>
   </div>
 </template>
 <script>
@@ -41,35 +41,36 @@ export default {
 };
 </script>
 <style>
+
   :root {
   --input-padding-x: 1.5rem;
   --input-padding-y: .75rem;
 }
-input::-webkit-input-placeholder {
-  color: transparent;
+.form-label-group>label {
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: block;
+  width: 100%;
+  margin-bottom: 0;
+  /* Override default `<label>` margin */
+  line-height: 1.5;
+  color: #495057;
+  border: 1px solid transparent;
+  border-radius: .25rem;
+  transition: all .1s ease-in-out;
 }
 
-input:-ms-input-placeholder {
+.form-label-group input::-webkit-input-placeholder {
   color: transparent;
 }
-input::-ms-input-placeholder {
-  color: transparent;
-}
-
-input::-moz-placeholder {
-  color: transparent;
-}
-
-input::placeholder {
-  color: transparent;
-}
-input:not(:placeholder-shown) {
+.form-label-group input:not(:placeholder-shown) {
   padding-top: calc(var(--input-padding-y) + var(--input-padding-y) * (2 / 3));
   padding-bottom: calc(var(--input-padding-y) / 3);
 }
 
-input:not(:placeholder-shown)~label {
-  padding-top: calc(var(--input-padding-y) / 10);
+.form-label-group input:not(:placeholder-shown)~label {
+  padding-top: calc(var(--input-padding-y) / 3);
   padding-bottom: calc(var(--input-padding-y) / 3);
   font-size: 12px;
   color: #777;
