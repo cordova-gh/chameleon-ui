86ddfc44dfc1abc67d0f6f3097de546639311975
geancarlocordova <geancarlo.cordova@kireygroup.com>
2020-09-19 20:46:50 +0200

    resolved the image search

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -57,7 +57,8 @@ module.exports = {
         loader: 'url-loader',
         options: {
           limit: 10000,
-          name: utils.assetsPath('img/[name].[hash:7].[ext]')
+          name: utils.assetsPath('img/[name].[hash:7].[ext]'),
+          esModule: false // <- here
         }
       },
       {
@@ -65,7 +66,8 @@ module.exports = {
         loader: 'url-loader',
         options: {
           limit: 10000,
-          name: utils.assetsPath('media/[name].[hash:7].[ext]')
+          name: utils.assetsPath('media/[name].[hash:7].[ext]'),
+          esModule: false // <- here
         }
       },
       {
@@ -73,7 +75,8 @@ module.exports = {
         loader: 'url-loader',
         options: {
           limit: 10000,
-          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+          name: utils.assetsPath('fonts/[name].[hash:7].[ext]'),
+          esModule: false // <- here
         }
       },
       {
