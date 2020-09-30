f4dd97cf8257a6218469ba0211b7678842e005b4
geancarlocordova <geancarlo.cordova@kireygroup.com>
2020-09-30 09:04:14 +0200

    inventariocarico

--- src/pages/company/inventario/inventario-movimento/inventario-carico/InventarioCarico.vue
+++ src/pages/company/inventario/inventario-movimento/inventario-carico/InventarioCarico.vue
@@ -1,89 +1,115 @@
 <template>
   <div>
-      <div class="container shadow p-2 mb-3 bg-white rounded">
+    <div class="container shadow p-2 mb-3 bg-white rounded">
       <div class="row">
         <div class="col">
-            <input-number
-              v-model="movimentoInventario['quantita']"
-              label="Quantita"
-            >
-            </input-number>
+          <input-number
+            v-model="movimentoInventario['quantita']"
+            label="Quantita"
+          >
+          </input-number>
         </div>
         <div class="col">
-           <input-date
-              v-model="movimentoInventario['dataScadenza']"
-              label="Data scadenza"
-            >
-            </input-date>
+          <input-date
+            v-model="movimentoInventario['dataScadenza']"
+            label="Data scadenza"
+          >
+          </input-date>
         </div>
         <div class="col">
-          <template >
-              <input-autocomplete
-                v-model="movimentoInventario['fornitore']"
-                :config="configTypes['fornitore']"
-                label="Fornitore "
-              >
-              </input-autocomplete>
-            </template>
+          <template>
+            <input-autocomplete
+              v-model="movimentoInventario['fornitore']"
+              :config="configTypes['fornitore']"
+              label="Fornitore "
+            >
+            </input-autocomplete>
+          </template>
         </div>
         <div class="col">
-           <input-text
-              v-model="movimentoInventario['note']"
-              label="Note"
-              fieldName="note"
-            >
-            </input-text>
+          <input-text
+            v-model="movimentoInventario['note']"
+            label="Note"
+            fieldName="note"
+          >
+          </input-text>
         </div>
         <div class="col-1">
           <i @click="saveEntity(indexEntity)" class="fa fa-check-circle"></i>
         </div>
       </div>
-      </div>
+    </div>
     <div class="container">
       <div class="accordion" id="accordionExample">
-        <div v-for="(inventarioMovimento, index) of inventarioMovimentos" :key="index">
-        <div class="card">
-          <div class="card-header" id="headingTwo">
-            <div class="d-flex justify-content-between align-items-center">
-              <h2 class="mb-0">
-                <button
-                  class="btn btn-link btn-block text-left collapsed"
-                  type="button"
-                  data-toggle="collapse"
-                  :data-target="'#collapse'+index"
-                  aria-expanded="false"
-                  :aria-controls="'collapse'+index"
+        <div
+          v-for="(inventarioMovimento, index) of inventarioMovimentos"
+          :key="index"
+        >
+          <div class="card">
+            <div class="card-header" id="headingTwo">
+              <div class="d-flex justify-content-between align-items-center">
+                <h2 class="mb-0">
+                  <button
+                    class="btn btn-link btn-block text-left collapsed"
+                    type="button"
+                    data-toggle="collapse"
+                    :data-target="'#collapse' + index"
+                    aria-expanded="false"
+                    :aria-controls="'collapse' + index"
+                  >
+                    {{
+                      inventarioMovimento.dataRegistrazione
+                        | formatDate("DD/MM/YYYY")
+                    }}
+                    / {{ movimentoInventario.note }}
+                  </button>
+                </h2>
+                <span class="badge badge-primary badge-pill">
+                  Disponibile {{ quantitas[inventarioMovimento._id].valore }} /
+                  {{ inventarioMovimento.quantita }}</span
                 >
-                 {{inventarioMovimento.dataRegistrazione | formatDate('DD/MM/YYYY')}}
-                 / {{movimentoInventario.note}}
-                </button>
-              </h2>
-              <span class="badge badge-primary badge-pill">
-                Disponibile {{quantitas[inventarioMovimento._id].valore}}
-                / {{inventarioMovimento.quantita}}</span>
+              </div>
             </div>
-          </div>
-          <div :id="'collapse'+index" class="collapse" aria-labelledby="headingTwo"
-          data-parent="#accordionExample">
-            <div class="card-body">
-              <div class="row"
-              v-for="(inventarioShopMovimento, idx) of inventarioMovimento.inventarioShopMovimentos"
-              :key="idx">
-                <div class="col"> {{inventarioShopMovimento.shop.codice}}</div>
-                <div class="col"> <input-number label="Quantita"
-                  v-model="inventarioShopMovimento.quantita"
-                @blur="calcolaDisponibilita(inventarioMovimento._id
-                , inventarioMovimento)">
-                  </input-number>
+            <div
+              :id="'collapse' + index"
+              class="collapse"
+              aria-labelledby="headingTwo"
+              data-parent="#accordionExample"
+            >
+              <div class="card-body">
+                <div
+                  class="row"
+                  v-for="(inventarioShopMovimento,
+                  idx) of inventarioMovimento.inventarioShopMovimentos"
+                  :key="idx"
+                >
+                  <div class="col">
+                    {{ inventarioShopMovimento.shop.codice }}
+                  </div>
+                  <div class="col">
+                    <input-number
+                      label="Quantita"
+                      v-model="inventarioShopMovimento.quantita"
+                      @blur="
+                        calcolaDisponibilita(
+                          inventarioMovimento._id,
+                          inventarioMovimento
+                        )
+                      "
+                    >
+                    </input-number>
+                  </div>
+                </div>
+                <div class="col">
+                  <input
+                    type="button"
+                    value="invia"
+                    @click="salvaMovimentiShops(inventarioMovimento)"
+                  />
                 </div>
               </div>
-               <div class="col">
-                   <input type="button" value="invia"
-                    @click="salvaMovimentiShops(inventarioMovimento)">
-                </div>
             </div>
           </div>
-        </div>
         </div>
       </div>
     </div>
@@ -92,7 +118,11 @@
 <script>
 import TitlePage from '@/pages/shared/components/TitlePage';
 import HttpCall from '@/services/HttpCall';
-import { API_INVENTARIO_MOVIMENTO, API_INVENTARIO_SHOP_MOVIMENTO, API_SHOP } from '@/services/constant-services';
+import {
+  API_INVENTARIO_MOVIMENTO,
+  API_INVENTARIO_SHOP_MOVIMENTO,
+  API_SHOP,
+} from '@/services/constant-services';
 import InputAutocomplete from '@/ui-components/input-components/InputAutocomplete';
 import InputText from '@/ui-components/input-components/InputText';
 import InputNumber from '@/ui-components/input-components/InputNumber';
@@ -167,38 +197,42 @@ export default {
     },
     loadEntities() {
       let params = '';
-      if (this.prodottoId !== '') params += `?articolo.equals=${this.prodottoId}`;
+      if (this.prodottoId !== '') {
+        params += `?articolo.equals=${this.prodottoId}`;
+      }
+      this.inventarioMovimentos = [];
       this.httpCallInventarioMov.get(params).then((data) => {
         const retInventarioMovimentos = data.entities;
         retInventarioMovimentos.forEach((inventarioMovimento) => {
           // eslint-disable-next-line no-console
           console.log(' da verificare');
           const inventarioShopMovimentosToSend = [];
+          // eslint-disable-next-line no-underscore-dangle
+          const idInventarioMovimento = inventarioMovimento._id;
 
-          // eslint-disable-next-line no-underscore-dangle
-          this.quantitas[inventarioMovimento._id] = { valore: inventarioMovimento.quantita };
-          // eslint-disable-next-line no-underscore-dangle
-          this.httpCallInventarioShopMov.getCustom(`/inventario-movimento/${inventarioMovimento._id}`)
+          this.httpCallInventarioShopMov
+            .getCustom(`/inventario-movimento/${idInventarioMovimento}`)
             .then((inventarioShopMovimentos) => {
-              // eslint-disable-next-line no-console
-              console.log('sjdhsjdhasjdhj');
-              this.shops.forEach((shop) => {
-                const inventarioShopMovimentoFound = inventarioShopMovimentos
-                  // eslint-disable-next-line no-underscore-dangle
-                  .filter(inventarioShopMovimento => inventarioShopMovimento.shop._id === shop._id);
+              this.shops.forEach((shopEntity) => {
+                const inventarioShopMovimentoFound = inventarioShopMovimentos.filter(
+                  inventarioShopMovimento =>
+                    // eslint-disable-next-line no-underscore-dangle
+                    inventarioShopMovimento.shop._id === shopEntity._id,
+                );
                 if (inventarioShopMovimentoFound.length > 0) {
-                  inventarioShopMovimentosToSend.push(inventarioShopMovimentoFound);
+                  inventarioShopMovimentosToSend.push(inventarioShopMovimentoFound[0]);
                 } else {
-                  const inventioShopMovimentoTemp = {
-                    quantita: 0,
-                    shop,
-                  };
+                  const inventioShopMovimentoTemp = { quantita: 0, shop: shopEntity };
                   inventarioShopMovimentosToSend.push(inventioShopMovimentoTemp);
                 }
               });
               // eslint-disable-next-line no-param-reassign
               inventarioMovimento.inventarioShopMovimentos = inventarioShopMovimentosToSend;
               this.inventarioMovimentos.push(inventarioMovimento);
+              this.quantitas[idInventarioMovimento] = {
+                valore: inventarioMovimento.quantita,
+              };
+              this.calcolaDisponibilita(idInventarioMovimento, inventarioMovimento);
             });
         });
         // this.inventarioMovimentos = retInventarioMovimentos;
@@ -218,10 +252,12 @@ export default {
       //   });
       // } else {
       this.movimentoInventario.articolo = this.prodottoId;
-      this.httpCallInventarioMov.create(this.movimentoInventario, '/save-carico').then(() => {
-        this.movimentoInventario = this.createEntityForm();
-        this.loadEntities();
-      });
+      this.httpCallInventarioMov
+        .create(this.movimentoInventario, '/save-carico')
+        .then(() => {
+          this.movimentoInventario = this.createEntityForm();
+          this.loadEntities();
+        });
       // }
     },
     salvaMovimentiShops(inventarioMovimento) {
@@ -232,9 +268,7 @@ export default {
         inventarioShopMovimentos: inventarioMovimento.inventarioShopMovimentos,
       };
 
-      this.httpCallInventarioMov.create(body, '/split-in-shops').then(() => {
-
-      });
+      this.httpCallInventarioMov.create(body, '/split-in-shops').then(() => {});
     },
     createEntityForm() {
       const obj = {};
@@ -243,7 +277,6 @@ export default {
       });
       return obj;
     },
-
   },
   watch: {
     prodottoId() {
--- src/ui-components/input-components/InputNumber.vue
+++ src/ui-components/input-components/InputNumber.vue
@@ -14,7 +14,7 @@
 export default {
   props: {
     value: {
-      type: String,
+      type: Number,
     },
     label: {
       type: String,
